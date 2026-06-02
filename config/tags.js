8e9bbfbfe04487cdecb2ede25a03aa290bb0a6c2
Emily57 <cookie0915n@gmail.com>
2026-06-02 22:05:21 +0900

    Expand background images and code tags lists for enhanced game content

--- config/tags.js
+++ config/tags.js
@@ -1,7 +1,38 @@
 // ---- タグ定義ファイル ----
 // ---- 背景画像リスト ----
 // ここにファイル名（拡張子なし）を追加・削除するだけで反映されます
-window.BgImages = ["white", "black", "classroom_front", "classroom_back"];
+window.BgImages = [
+  "white",
+  "black",
+  "classroom_front",
+  "classroom_back",
+  "harakubo_execution_hall",
+  "waiting_room",
+  "private_room",
+  "you_are_cleric",
+  "corridor",
+  "blood_splatter",
+  "warehouse",
+  "entrance",
+  "dining_room",
+  "city_station_square",
+  "stairs",
+  "library",
+  "reference_room",
+  "entertainment_room",
+  "school_warehouse",
+  "mito_execution",
+  "admin",
+  "city",
+  "school_gym",
+  "school_gym_up",
+  "sky",
+  "school_entrance",
+  "city_station_square",
+  "hospital",
+  "hall_above",
+  "hall",
+];
 
 // ---- BGM リスト ----
 // ここにファイル名（拡張子なし）を追加・削除するだけで反映されます
@@ -21,4 +52,8 @@ window.BgmList = [
 
 // ---- コードタグ候補 ----
 // テーブルの code プルダウンに表示されます
-window.CodeTags = ["[sepia_start]"];
+window.CodeTags = [
+  '[sepia_start bg_key="xxx"]',
+  "[sepia_end]",
+  '[fadeoutbgm time="5000"]',
+];
